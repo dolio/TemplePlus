cc1af0fc1e26ea987b7bab004df3d83807b54bd4
Dan Doel <dan.doel@gmail.com>
2024-01-29 19:16:36 -0500

    Log a different text drawing function

--- TemplePlus/ui/ui_render.cpp
+++ TemplePlus/ui/ui_render.cpp
@@ -130,7 +130,6 @@ bool UiRenderer::DrawText(const string& text, const TigRect& rect, const TigText
 	if (text.empty())
 		return 1;
 
-	logger->info("Drawing: {}", text);
 	return tigFont.Draw(text.c_str(), rect, style) == TRUE;
 }
 
@@ -141,6 +140,7 @@ bool UiRenderer::DrawTextInWidget(int widgetId, const string &text, const TigRec
 	if (text.empty())
 		return 1;
 
+	logger->info("Drawing: {}", text);
 	TigRect extents(rect.x + wid->x , rect.y + wid->y , rect.width,rect.height);
 
 	return tigFont.Draw(text.c_str(), extents, style) == TRUE;
