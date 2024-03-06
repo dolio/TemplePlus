82ee01893fba373d28ba9dd967eeeb48fed2b108
Dan Doel <dan.doel@gmail.com>
2024-03-06 02:41:46 -0500

    Remove vrock logging

--- TemplePlus/condition.cpp
+++ TemplePlus/condition.cpp
@@ -5080,9 +5080,6 @@ int SpellCallbacks::VrockSporesCountdown(DispatcherCallbackArgs args)
 	auto hasDelay = d20Sys.d20QueryWithData(target, DK_QUE_Critter_Has_Condition, delay, 0);
 	auto strict = config.stricterRulesEnforcement;
 
-	auto istr = fmt::format("VrockSporesCountdown !!delay: {}, hasDelay: {}", !!delay, hasDelay);
-	logger->info(istr);
-
 	// delay poison is only postponing the countdown
 	if (strict && hasDelay) return 0;
 
